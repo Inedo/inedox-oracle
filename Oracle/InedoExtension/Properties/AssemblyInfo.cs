6ffc80df8dc29dbf90d12d9111e702e44aaa66fd
Ben Lubar <ben.lubar@gmail.com>
2018-05-15 11:14:42 -0500

    Allow running tests in release mode for appveyor.

--- Oracle/InedoExtension/Properties/AssemblyInfo.cs
+++ Oracle/InedoExtension/Properties/AssemblyInfo.cs
@@ -8,11 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
 [assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
-#if DEBUG
 [assembly: InternalsVisibleTo("OracleUnitTests")]
-#endif
