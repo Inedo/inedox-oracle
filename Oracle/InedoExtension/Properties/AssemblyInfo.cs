63f4554dc464349b77680e730dd7d4cad2db54b8
Greg Divis <gdivis@inedo.com>
2024-10-24 10:37:05 -0400

    update sdk

--- Oracle/InedoExtension/Properties/AssemblyInfo.cs
+++ Oracle/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Inedo.Extensibility;
 
@@ -10,10 +9,8 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Database integration for Oracle 9i and later.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
-
-[assembly: InternalsVisibleTo("OracleUnitTests")]
